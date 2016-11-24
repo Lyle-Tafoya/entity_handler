3d725989e4cf32038a8a32b8f50c53e69aac1ed4
Lyle Tafoya <lyle.tafoya@gmail.com>
2016-11-24 00:07:09 -0800

    Added component and entity loading

--- examples/hello_world/hello_world.rb
+++ examples/hello_world/hello_world.rb
@@ -1,6 +1,8 @@
 require_relative 'systems/graphics_ncurses'
 
 begin
+  EntityHandler::System.load_components('components/')
+  EntityHandler::System.load_entities('entities/')
   graphics_system = EntityHandler::GraphicsNcurses.new()
   sleep(2)
 ensure
--- lib/entity_handler.rb
+++ lib/entity_handler.rb
@@ -2,27 +2,27 @@ require "entity_handler/version"
 
 # The EntityHandler module will contain all classes needed to begin developing simulation software
 module EntityHandler
+  require 'json'
 
-  # A system is generic class which defines a standardized method of communication.
-  # Systems communicate by broadcasting messages consisting of key-value pairs. 
-  # A type_id is an identifier used to index an array or map to trigger a callback function to handle the message.
+  # A system is a generic class which defines a standardized method of communication.
+  # Systems communicate by broadcasting and listening for messages consisting of key-value pairs. 
   #
   # @author Lyle Tafoya
+  # @abstract
   class System
 
-    # A map where the key is message type_id and value is array of System callback methods
+    # Map where keys are message type_ids and values are arrays of System callback methods
     @@callback_registry = {}
 
-    # Map consisting of entities and the components of which they are comprised.
+    # Map where keys are entity names and values are names of the components of which they are comprised.
     # Used as templates for creating new entities
     @@entities = {}
 
-    # Map where keys are a component identifier and values are Maps representing the component data. 
+    # Map where keys are component identifiers and values are Maps representing the component data. 
     # Used as templates for creating new components
     @@components = {}
 
     # Register a callback method to be run when a particular type of message is broadcasted
-    # 
     # @param type_id [Integer]
     # @param callback [Method]
     def self.register_callback(type_id, callback)
@@ -31,22 +31,24 @@ module EntityHandler
     end
 
     # Run all callback methods registered for the message type_id
-    #
     # @param message [Hash]
     def self.broadcast_message(message)
       @@callback_registry[message['type_id']].each {|callback| callback.call(message) }
     end
 
     # Listen for incoming messages over the network on a particular port
-    #
     # @param port [Integer]
     def self.listen(port)
     end
 
     # Scan a directory and load all components into memory
-    #
     # @param directory_path [String]
     def self.load_components(directory_path)
+      Dir.foreach(directory_path) do |filename|
+        next unless filename.include?('.json')
+        component_name = filename.split(/\.json$/)
+        @@components[component_name] = JSON.parse(File.read(directory_path + filename))
+      end
     end
 
     # Clear the components map
@@ -55,16 +57,19 @@ module EntityHandler
     end
 
     # Remove a component from the components map
-    #
     # @param component [String]
     def self.unload_component(component)
       @@components.delete(component) if @@components.key?(component)
     end
 
     # Scan a directory and load all entities into memory
-    #
     # @param directory_path [String]
     def self.load_entities(directory_path)
+      Dir.foreach(directory_path) do |filename|
+        next unless filename.include?('.json')
+        entity_name = filename.split(/\.json$/)
+        @@entities[entity_name] = JSON.parse(File.read(directory_path + filename))
+      end
     end
 
     # Clear the entities map
@@ -73,10 +78,9 @@ module EntityHandler
     end
 
     # Remove an entity from the entity map
-    #
     # @param entity [String]
     def self.unload_entity(entity)
-      @@entities.delee(entity) if @@entities.key?(entity)
+      @@entities.delete(entity) if @@entities.key?(entity)
     end
 
   end
